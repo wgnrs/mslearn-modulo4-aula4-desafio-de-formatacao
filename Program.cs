86c29c95e6232b66ab2f30187d712266253eb779
Wagner <wagner_silvajr@hotmail.com>
2024-04-16 09:36:51 -0300

    finalizacao do desafio

--- Program.cs
+++ Program.cs
@@ -9,12 +9,22 @@ string newProduct = "Glorious Future";
 decimal newReturn = 0.13125m;
 decimal newProfit = 63000000.0m;
 
-// Your logic here
+System.Console.WriteLine($"Dears {customerName}");
+System.Console.WriteLine($"As as customer of {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+System.Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P2}\n");
+System.Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}\n");
 
 Console.WriteLine("Here's a quick comparison:\n");
 
 string comparisonMessage = "";
 
-// Your logic here
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
 
 Console.WriteLine(comparisonMessage);
--- obj/Debug/net8.0/mslearn-modulo4-aula4-desafio-de-formatacao.AssemblyInfo.cs
+++ obj/Debug/net8.0/mslearn-modulo4-aula4-desafio-de-formatacao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mslearn-modulo4-aula4-desafio-de-formatacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c50ca54371db0f2d17d940b3827b5910681819b")]
 [assembly: System.Reflection.AssemblyProductAttribute("mslearn-modulo4-aula4-desafio-de-formatacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mslearn-modulo4-aula4-desafio-de-formatacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
